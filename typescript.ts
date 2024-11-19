1f5fa315ab03aceb284a74547970065379365b31
ChiaraB96 <chiaraborgatti.96@gmail.com>
2024-11-19 12:48:52 -0300

    primer clase typescript

--- typescript.ts
+++ typescript.ts
@@ -13,37 +13,76 @@
 // Declarar una variable de tipo number llamada "edad" y asignarle tu edad.
 // Declarar una variable de tipo boolean llamada "esEstudiante" y asignarle true o false.
 // Imprimir en consola el valor de cada variable usando console.log()
+let nombre: string = "Chiara";
+let edad: number = 28;
+let esEstudiante: boolean = true;
 
-
-
+console.log(nombre,edad,esEstudiante)
 // Ejercicio 2
 // Declarar una función que reciba dos números como parámetros y retorne su suma.
 // Asegurar que los parámetros y el valor de retorno sean de tipo number.
 // Llamar a la función e imprimir el resultado.
 // Realizar otra función que reste dos números.
 // Realizar otra función que multiplique dos números.
-
+function sumar (a:number, b:number): number {
+    return a + b;
+}
+console.log('Suma: ', sumar(15,5))
+function restar (a:number, b:number): number {
+    return a - b;
+}
+console.log('Resta: ', restar(15,5))
+function multiplicar (a:number, b:number): number {
+    return a * b;
+}
+console.log('Multiplicacion: ', multiplicar(2,5))
 
 
 // Ejercicio 3
 // Crear un array de números llamado "numeros" y agregar algunos valores.
 // Usa un bucle para imprimir cada número multiplicado por 2 (usando forEach())
+let numeros: number [] = [1,2,3,4]
 
+numeros.forEach(num =>{console.log(multiplicar(num,2));})
 
 
 // Ejercicio 4
 // Declarar una interfaz "Persona" con las propiedades: nombre (string), edad (number), y esEstudiante (boolean).
 // Crear tres objetos que sigan la estructura de la interfaz.
 // Imprimir en consola.
+interface Persona {
+    nombre: string;
+    edad: number;
+    esEstudiante: boolean;
+}
 
+const persona: Persona[] =[{
+    nombre: 'Chiara',
+    edad: 28,
+    esEstudiante: true,
+},
+{
+    nombre: 'Juli',
+    edad: 20,
+    esEstudiante: true,
+},
+{
+    nombre: 'Sol',
+    edad: 30,
+    esEstudiante: false,
+}]
 
+console.log(persona)
 
 // Ejercicio 5
 // Declara una variable que pueda ser de tipo string o number.
 // Asigna un valor string y luego cambia su valor a number.
 // Imprimir en consola ambos casos.
-
-
+let valor: string|number;
+valor = 'Hola';
+console.log(valor);
+valor = 10;
+console.log(valor);
 
 // Ejercicio 6
 // Definir una interfaz "Producto" con propiedades: nombre (string), precio (number), y enStock (boolean).
@@ -51,6 +90,38 @@
 // Crear una función que reciba el array y retorne los productos en stock (sólo en stock). 
 // Crear otra función que reciba el array y retorne los productos sin stock (sólo sin stock). 
 // Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+interface Producto {
+    nombre: string,
+    precio: number,
+    enStock: boolean
+}
+
+const productos: Producto[] = [
+    {nombre: 'Telefono',
+    precio: 100,
+    enStock:true},
+    {nombre: 'Tablet',
+    precio: 150,
+    enStock:false},
+    {nombre: 'Teclado',
+    precio: 50,
+    enStock:true},
+    {nombre: 'PC',
+    precio: 1000,
+    enStock:true},
+    {nombre: 'Audifonos',
+    precio: 80,
+    enStock:true},
+    {nombre: 'Mouse',
+    precio: 10,
+    enStock:false}
+]
+function enStock(array:Producto[]): Producto[] {
+    return array.filter((productos) => productos.enStock = true)
+}
+function sinStock(array:Producto[]): Producto[] {
+    return array.filter((productos) => productos.enStock = false)
+}
 
 
 // Ejercicio 7
@@ -60,114 +131,35 @@
 // Crear otra función que reciba el array y retorne los clientes que cumplan la condición inversa al punto de arriba (mismo booleano que hayamos elegido). 
 // Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
 
-/*
-
-
-Ejercicio 8: Catálogo de Productos - forEach
-Crear un array llamado catalogo con varios objetos de productos. Cada producto debe tener las propiedades nombre (string) y precio (number).
-Usar forEach para imprimir el nombre y el precio de cada producto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
-
-
-Ejercicio 9: Catálogo de Productos - filter
-Utilizar filter para crear un nuevo array llamado productosBaratos que solo contenga los productos con precio menor a 50. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
-Imprimor productosBaratos en la consola.
-
-
-Ejercicio 10: Actualización de Inventario - map
-Utilizar map para crear un nuevo array catalogoConDescuento, donde cada producto tenga un 10% menos de precio. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
-Imprimir catalogoConDescuento en la consola.
-
-
-Ejercicio 11: Búsqueda de Usuario - find
-Crear un array llamado usuarios con varios objetos de usuario. Cada usuario debe tener id (number), nombre (string) y activo (boolean).
-Usar find para buscar el usuario con el id 3. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find 
-Imprimir en la consola el objeto del usuario encontrado.
-
-
-Ejercicio 12: Contador de Usuarios Activos - filter y length
-Utilizar filter para crear un nuevo array usuariosActivos que contenga solo los usuarios con activo: true.
-Usar la propiedad length para contar cuántos usuarios activos hay y muestra el resultado en la consola. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length 
-
-
-Ejercicio 13: Actualización de Estado de Usuarios
-Usando un bucle forEach, cambiar la propiedad activo de cada usuario a false.
-Imprimir el array usuarios para verificar que todos los usuarios están inactivos.
-
-
-Ejercicio 14: Formateo de Productos para Mostrar - map
-Usar el array catalogo.
-Utilizar map para crear un nuevo array productosFormato que contenga el nombre y precio de cada producto como un string en el formato "Producto: [nombre], Precio: $[precio]".
-Para esto podemos usar template strings `string text ${expression} string text`: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals 
-Imprimir el array productosFormato en la consola.
-
-
-
-
-
-Ejercicio 15:
-Con los datos que tenemos, crear una interfaz "User" y aplicarla, para que el siguiente codigo compile sin errores:
-
-
-const users: unknown[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    }
-];
-
-function logPerson(user: unknown) {
-    console.log(` - ${user.name}, ${user.age}`);
+interface Cliente {
+    nombre: string,
+    id: number,
+    estado: boolean
 }
 
-console.log('Users:');
-users.forEach(logPerson);
-
-
-
-
-Ejercicio 16:
-Vamos a volver a usar la interfaz User del ejercicio anterior.
-Crear una nueva interfaz Admin segun los datos que tenemos.
-Corregir el type Person para que acepte dos tipos: User y la nueva interfaz. 
-Corregir la implementacion para aplicar el type Person para que el siguiente codigo compile sin errores. 
-
-type Person = User;
-const persons: User[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
-];
-
-function logPerson(user: User) {
-    console.log(` - ${user.name}, ${user.age}`);
+const clientes: Cliente[] = [
+    {nombre: 'Telefono',
+    id: 1,
+    estado:true},
+    {nombre: 'Tablet',
+    id: 2,
+    estado:false},
+    {nombre: 'Teclado',
+    id: 3,
+    estado:true},
+    {nombre: 'PC',
+    id: 4,
+    estado:false},
+    {nombre: 'Audifonos',
+    id: 5,
+    estado:true},
+    {nombre: 'Mouse',
+    id: 6,
+    estado:false}
+]
+function activos(array:Cliente[]): Cliente[] {
+    return array.filter((clientes) => clientes.estado = true)
 }
-
-persons.forEach(logPerson);
-
-
-*/
-
-
+function inactivos(array:Cliente[]): Cliente[] {
+    return array.filter((clientes) => clientes.estado = false)
+}
